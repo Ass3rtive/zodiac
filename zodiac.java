131ec1974a2292045c440c3efba4b6103f78c873
Ass3rtive <130934423+Ass3rtive@users.noreply.github.com>
2023-05-31 13:42:14 +0300

    Create zodiac.java

---
+++ zodiac.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+public class zodiac {
+    public static void main(String[] args) {
+
+        int yil,hesaplama;
+        Scanner input= new Scanner(System.in);
+
+        System.out.print("Doğum Yılınızı Giriniz : ");
+        yil = input.nextInt();
+        hesaplama = yil%12;
+
+        switch (hesaplama){
+            case 0:
+                System.out.print("Çin Zodyağı Burcunuz : At");
+                break;
+            case 1:
+                System.out.print("Çin Zodyağı Burcunuz : Horoz");
+                break;
+            case 2:
+                System.out.print("Çin Zodyağı Burcunuz : Köpek");
+                break;
+            case 3:
+                System.out.print("Çin Zodyağı Burcunuz : Domuz");
+                break;
+            case 4:
+                System.out.print("Çin Zodyağı Burcunuz : Fare");
+                break;
+            case 5:
+                System.out.print("Çin Zodyağı Burcunuz : Öküz");
+                break;
+            case 6:
+                System.out.print("Çin Zodyağı Burcunuz: Kaplan");
+                break;
+            case 7:
+                System.out.print("Çin Zodyağı Burcunuz : Tavşan");
+                break;
+            case 8:
+                System.out.print("Çin Zodyağı Burcunuz : Ejderha");
+                break;
+            case 9:
+                System.out.print("Çin Zodyağı Burcunuz : Yılan");
+                break;
+            case 10:
+                System.out.print("Çin Zodyağı Burcunuz: At");
+                break;
+            case 11:
+                System.out.print("Çin Zodyağı Burcunuz : Koyun");
+                break;
+            /**
+             * @author Erdem AYDEMİR
+             */
+
+
+
+        }
+
+    }
+}
